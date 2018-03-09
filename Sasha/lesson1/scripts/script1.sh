d67ccd7f1800a771f79a0c540a17a04333f0a3b3
Sasha <Sashakingdoms@gmail.com>
2018-03-09 19:52:05 +0400

    Added scripts

---
+++ Sasha/lesson1/scripts/script1.sh
@@ -0,0 +1,15 @@
+cd ../output
+rm -rf u1
+mkdir u1
+cd u1
+mkdir u2 u3
+touch a.txt
+chmod 777 a.txt
+echo London is the capital of Great Britain >a.txt
+cd u2
+touch b.txt c.txt
+chmod 777 b.txt
+chmod 777 c.txt
+echo Moscow is the capital of Russia >b.txt
+echo Yeravan is the capital of Armenia >c.txt
+
---
+++ Sasha/lesson1/scripts/script2.sh
@@ -0,0 +1,3 @@
+echo "u1:a.txt $(more ../output/u1/a.txt)"
+echo "u2:b.txt $(more ../output/u1/u2/b.txt)"
+echo "u2:c.txt $(more ../output/u1/u2/c.txt)"
